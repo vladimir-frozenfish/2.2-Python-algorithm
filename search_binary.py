cbd8baa91619cf2bc98ce70aeda7e2e259b77932
Vladimir_Kirichenko <frozenfish@internet.ru>
2021-10-30 09:56:52 +0300

    добавлен скрипт бинарного посика - search_binary.py

---
+++ search_binary.py
@@ -0,0 +1,46 @@
+'''
+Функция бинарного поиска.
+В функцию подается отсортированный массив и ключ.
+Функция возращает левую и правую границу ключа (ключей) в массиве
+'''
+
+def left_bound(A: list, key):
+    left = -1
+    right = len(A)
+
+    while right - left > 1:
+        middle = (right + left) // 2
+        if A[middle] < key:
+            left = middle
+        else:
+            right = middle
+
+    return left
+
+
+def right_bound(A: list, key):
+    left = -1
+    right = len(A)
+
+    while right - left > 1:
+        middle = (right + left) // 2
+        if A[middle] <= key:
+            left = middle
+        else:
+            right = middle
+
+    return right
+
+def search_binary(A: list, key):
+    A = A
+    key = key
+    search_result = [0, 0]
+    search_result[0], search_result[1] = left_bound(A, key), right_bound(A, key)
+    return search_result
+
+
+A = [1,1,1,4,4,4,5,7,8,10,11,23,25]
+print(search_binary(A, -5))
+
+
+
--- temp.py
+++ temp.py
@@ -11,12 +11,19 @@ print(stroka_reverse)
 '''
 
 # реверсируем список от определенного элемента
-n = 6
-spisok = [1,2,3,4,5,6,7,8]
-
-spisok_reverse = spisok[0: n+1] + spisok [-1: n: -1]
+n = 4
+spisok = [1, 2, 3, 4, 5, 6, 7, 8]
+spisok_reverse = spisok[0: n+1] + spisok[-1: n: -1]
 print(spisok_reverse)
 
+# реверсируем строку от определенного элемента
 stroka = '123abc'
-stroka_reverse = stroka[0:3] + stroka[-1 : 2 : -1]
+stroka_reverse = stroka[0:3] + stroka[-1: 2: -1]
 print(stroka_reverse)
+
+
+# перемена местами в строке первого с последним элементом
+stroka = 'abcdef'
+print(stroka)
+stroka = stroka [-1] + stroka[1: -1] + stroka[0]
+print(stroka)
